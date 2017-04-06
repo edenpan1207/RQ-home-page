6fa96ad5b5dc1ab0329b573aa609f14d4143985e
edenpan1207 <xadam69@gmail.com>
2017-04-06 14:12:18 +0800

    upload file

--- js/home.js
+++ js/home.js
@@ -1,59 +1,7 @@
-$(document).ready(function() {
-    $(".center").slick({
+$(document).on('ready', function() {
+    $(".variable").slick({
         dots: true,
         infinite: true,
-        centerMode: true,
-        variableWidth: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
+        variableWidth: true
     });
-
-    var the_terms = $("#checkbox-submit");
-    the_terms.click(function() {
-        if ($(this).is(":checked")) {
-            $("#submit").removeAttr("disabled").css('opacity', '1');
-        } else {
-            $("#submit").attr("disabled", "disabled").css('opacity', '0.5');
-        }
-    });
-
-    $('#coach').on('change', function() {
-        var val = $(this).val();
-        $('[data-coach]').show();
-        $.each($('[data-coach]'), function() {
-            console.log($(this));
-            if ($(this).data('coach') !== val) {
-                $(this).parent().parent().hide();
-            } else {
-                $(this).parent().parents().show();
-            }
-        });
-    });
-
-    $('#location').on('change', function() {
-        var val = $(this).val();
-        $('[data-location]').show();
-        $.each($('[data-location]'), function() {
-
-            if ($(this).data('location') !== val) {
-                $(this).parent().parent().parent().hide();
-            } else {
-                $(this).parent().parent().parents().show();
-            }
-        });
-    });
-
-    $('#time').on('change', function() {
-        var val = $(this).val();
-        $('[data-time]').show();
-        $.each($('[data-time]'), function() {
-            console.log($(this));
-            if ($(this).data('time') !== val) {
-                $(this).parent().parent().parent().hide();
-            } else {
-                $(this).parent().parent().parents().show();
-            }
-        });
-    });
-
 });
